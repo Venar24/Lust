378fd0b8b213a6bf6b0e674bc1416cc414578ee5
Venar <venar.oathbreaker@outlook.com>
2024-11-21 02:16:32 -0500

    added -c argument to see hidden files

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
+use std::env;
 use std::fs;
 use std::io;
 
 fn main() -> io::Result<()> {
-    // Get the current directory
-    let current_dir = std::env::current_dir()?;
-    //println!("Current directory: {}", current_dir.display());
+
+    let args: Vec<String> = env::args().collect();
+    let show_hidden = args.contains(&"-c".to_string());
 
     // Read and list the contents of the current directory
-    for entry in fs::read_dir(current_dir)? {
+    for entry in fs::read_dir(std::env::current_dir()?)? {
         let entry = entry?;
         let file_name = entry.file_name();
         let file_name_str = file_name.to_string_lossy();
 
         // Skip hidden files (file names starting with '.')
-        if !file_name_str.starts_with('.') {
+        if show_hidden || !file_name_str.starts_with('.') {
             println!("{}", file_name_str);
         }
     }
